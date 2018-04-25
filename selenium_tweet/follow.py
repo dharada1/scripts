6d0d40ccfd939757a64c9c9d59684eaaf85a61a0
dharada1 <haradadaichi715@gmail.com>
2018-04-26 01:32:45 +0900

    from secrets

--- selenium_tweet/follow.py
+++ selenium_tweet/follow.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from selenium import webdriver
  
-def post_twitter( user_name, password):
+def post_twitter( user_name, password, user_id):
    # chromedriver落としてきて同じディレクトリに配置。
    browser = webdriver.Chrome(executable_path="./chromedriver")
    browser.get("https://twitter.com/")
@@ -13,21 +13,16 @@ def post_twitter( user_name, password):
    pass_wd.send_keys(password)
    pass_wd.submit()
  
-   # ツイートの処理
-   from datetime import datetime
-   post_body = browser.find_element_by_id("tweet-box-home-timeline")
-   post_body.send_keys("Python test: "+datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-   try:
-      post_button = browser.find_element_by_css_selector("button.tweet-action")
-      post_button.click()
-      print("ツイート成功")
-   except:
-      print("ツイート失敗")
- 
+   browser.get("https://twitter.com/" + user_id)
    browser.close()
  
 if __name__ == "__main__":
    from getpass import getpass
-   name = input("user name : ")
-   pw = getpass("password  : ")
-   post_twitter(name, pw)
+   user_id = input("who to open :")
+
+
+   test_data = open(".secrets", "r")
+   contents = test_data.read().split(" ")
+   test_data.close()
+
+   post_twitter(contents[0], contents[1], user_id)
